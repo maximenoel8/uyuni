b57c0693a1a1892615afe31e37792131043f6ea8
Maxime Noel <maxime.noel@suse.com>
2021-07-13 14:34:00 +1200

    Upload vCenter.json file from testsuite to server

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -1620,3 +1620,9 @@ When(/^I copy autoinstall mocked files on server$/) do
   return_codes << file_inject($server, base_dir + 'sles15sp2/linux', source_dir + 'SLES15-SP2-x86_64/DVD1/boot/x86_64/loader/linux')
   raise 'File injection failed' unless return_codes.all?(&:zero?)
 end
+
+And(/^I copy vcenter configuration file on server$/) do
+  base_dir = File.dirname(__FILE__) + "/../upload_files/virtualization/"
+  return_code = file_inject($server, base_dir + 'vCenter.json', '/var/tmp/vCenter.json')
+  raise 'File injection failed' unless return_code.zero?
+end
