5c3510ffaeb0522afd98b9d42d1b2286d7e20774
Maxime Noel <maxime.noel@suse.com>
2024-11-15 11:35:28 +1300

    Show results

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -579,6 +579,8 @@ When(/^the server starts mocking an IPMI host$/) do
   end
   server.run('chmod +x /etc/ipmi/fake_ipmi_host.sh', verbose: true, check_errors: true)
   # Check if ipmi_sim is already running
+  result = server.run('pgrep -f ipmi_sim', verbose: false, check_errors: false)
+  puts "ipmi_sim status #{result}"
   if server.run('pgrep -f ipmi_sim', verbose: true, check_errors: false).empty?
     server.run('ipmi_sim -n < /dev/null > /dev/null &', verbose: true, check_errors: true)
   else
