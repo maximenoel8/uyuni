5b6753be7f8d6e96cba12349ee9487df0ad79f66
Maxime Noel <maxime.noel@suse.com>
2024-11-15 11:34:08 +1300

    Change for verbose true

--- testsuite/features/step_definitions/command_steps.rb
+++ testsuite/features/step_definitions/command_steps.rb
@@ -579,7 +579,7 @@ When(/^the server starts mocking an IPMI host$/) do
   end
   server.run('chmod +x /etc/ipmi/fake_ipmi_host.sh', verbose: true, check_errors: true)
   # Check if ipmi_sim is already running
-  if server.run('pgrep -f ipmi_sim', verbose: false, check_errors: false).empty?
+  if server.run('pgrep -f ipmi_sim', verbose: true, check_errors: false).empty?
     server.run('ipmi_sim -n < /dev/null > /dev/null &', verbose: true, check_errors: true)
   else
     puts 'ipmi_sim is already running; skipping startup.'
